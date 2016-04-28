d97f09bc825381e561ec61349a196d4448be9318
rosario <rhc02014@pomona.edu>
2016-04-27 22:29:15 -0700

    idk

--- Assignment11_Huamani_Carpio_Rosario_Cevallos_Sebastian/src/binary_tree_io.c
+++ Assignment11_Huamani_Carpio_Rosario_Cevallos_Sebastian/src/binary_tree_io.c
@@ -10,12 +10,11 @@ struct binary_tree_io {
 
 };
 
-void binary_tree_write (binary_tree* self, FILE* stream){
+void binary_tree_write(binary_tree* self, FILE* stream){
   //print string of tree in pre order recursively
 
-  if (binary_tree_get_left(self) == NULL && binary_tree_get_right(self) == NULL){
+  if (binary_tree_get_left(self) == NULL && binary_tree_get_right(self)== NULL){
     char line[1000];
-
     binary_tree_get_string(self, line);
     fprintf(stream, "%s", line);
   }else {
@@ -26,10 +25,6 @@ void binary_tree_write (binary_tree* self, FILE* stream){
     binary_tree_write(binary_tree_get_right(self), stream);
 
   }
-
-
-
-
 }
 
 binary_tree* binary_tree_create_f(FILE* stream){
@@ -38,6 +33,7 @@ binary_tree* binary_tree_create_f(FILE* stream){
   // if there's a blank line in input file
   // other unexpected errors
 
+  binary_tree* new_tree = binary_tree_create();
   size_t len;
   char *line = fgetln(stream, &len);
   // if line starts with Q or A and is not a blank linegi
