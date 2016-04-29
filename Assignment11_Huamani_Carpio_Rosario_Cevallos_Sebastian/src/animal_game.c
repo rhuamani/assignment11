0d8a9ec74018e757cc85afdea2aa6e2670a8cd8a
rosario <rhc02014@pomona.edu>
2016-04-28 21:44:48 -0700

    noah said stream would update accordingly so it'll never be reading the same line when calling recursively.. so i just commented out some things, however, we dont use helper methods.. are we missing something?

--- Assignment11_Huamani_Carpio_Rosario_Cevallos_Sebastian/src/animal_game.c
+++ Assignment11_Huamani_Carpio_Rosario_Cevallos_Sebastian/src/animal_game.c
@@ -4,8 +4,13 @@
 #include "binary_tree.h"
 #include "binary_tree_io.h"
 
+struct animal_game {
 
-void play(char *input, FILE* stream, char *output ){
+
+};
+
+
+void play(char* input, FILE* stream, char* output ){
 
 
 
