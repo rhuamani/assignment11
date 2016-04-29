a74a9bfda2568adec22d34686831f74ec4b92323
rosario <rhc02014@pomona.edu>
2016-04-28 22:41:13 -0700

    copied bailey, havent done part that doesnt make sense to me

--- Assignment11_Huamani_Carpio_Rosario_Cevallos_Sebastian/src/animal_game.c
+++ Assignment11_Huamani_Carpio_Rosario_Cevallos_Sebastian/src/animal_game.c
@@ -8,11 +8,80 @@ struct animal_game {
 
 
 };
+bool affirmative_answer(char* str){
+  char first = *str;
+  return first == 'y';
 
+}
 
 void play(char* input, FILE* stream, char* output ){
+  //open input file for reading
+  FILE *input_file = fopen(input, "r");
+
+  binary_tree* tree = binary_tree_create_f(input_file);
+
+  size_t len;
+  char* answer = fgetln(stream, &len);
+  //while person wants to play
+  while (affirmative_answer(answer) ){
+    //play round
+
+    answer= fgetln(stream, &len);
+
+  }
+
+}
+
+binary_tree* play_round(FILE* stream, binary_tree* tree){
+
+  char* answer = NULL;
+  binary_tree* root = tree;
+
+  while (!binary_tree_is_leaf(tree)){
+
+    char* str = NULL;
+    binary_tree_get_string(tree, str);
+    printf("%s\n", str );
+    size_t len;
+    answer = fgetln(stream, &len);
+
+    if(affirmative_answer(answer)){
+      tree = binary_tree_get_right(tree);
+
+    }else {
+      tree= binary_tree_get_left(tree);
+    }
 
 
+  }
+  //check if guess was correct
+  char* guess= NULL;
+  binary_tree_get_string(tree, guess);
+  printf("Were you thinking of a %s\n ?",guess);
+  size_t len;
+  char* final_answer= fgetln(stream, &len);
 
+  if (affirmative_answer(final_answer)){
+
+    //game was guessed, its over
+    printf("GOALS");
+    return root;
+  }else {
+    printf("Wow I'm dumb...what was the animal?");
+    char* animal = fgetln(stream, &len);
+    printf("What question separates %s\n from %s\n ?", animal,guess );
+    char* question = fgetln(stream, &len);
+    printf("What is the correct answer for this animal?");
+    char* correct_answer = fgetln(stream, &len);
+
+
+    binary_tree* parent = NULL;
+
+    //something here is checkde but its a lil weird i dont get it
+    //
+
+  }
+
+  return NULL;
 
 }
